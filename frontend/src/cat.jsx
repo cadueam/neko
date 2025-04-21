c57a2171f362c3e9ae0bda41971245256687c612
Cadu Andrade <cadu.eam@gmail.com>
2025-04-21 19:29:27 -0300

    Learning react comp amazing

---
+++ frontend/src/cat.jsx
@@ -0,0 +1,15 @@
+function Catbox() {
+  return (
+    <div>
+      <h1>NomeGato</h1>
+
+      <img
+        className="size-25 rounded-full"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F617278%2Fpexels-photo-617278.jpeg%3Fcs%3Dsrgb%26dl%3Danimal-cute-eyes-617278.jpg%26fm%3Djpg&f=1&nofb=1&ipt=e5d00ce336cc53a5971704ee698a26e33793140d468b549b2efeb37fa32b5130"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Catbox;
